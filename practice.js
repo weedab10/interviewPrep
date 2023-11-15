80a06c78dbc1fc196ea69b8d3be2b3d547ead2fd
weedab10 <ragweed-hoaxer-0v@icloud.com>
2023-11-14 21:40:49 -0600

    Problems 1-3 Prject 13

---
+++ practice.js
@@ -0,0 +1,15 @@
+// function reverseString(str) {
+//     return str.split("").reverse().join("");
+// }
+// reverseString("world");
+
+function reverse(str) {
+    var newString = "";
+
+    for (var i = newString.length -1; i >=0; i--) {
+        newString += str[i];
+    }
+        return newString;
+}
+
+reverse("world");
